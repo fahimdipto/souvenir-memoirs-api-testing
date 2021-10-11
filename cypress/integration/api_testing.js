76d6445b08be1e2bf4408c3916be374031075a6e
fahimdipto <sultanparvez77.sp@gmail.com>
2021-10-12 03:20:55 +0600

    Arrangement naming convection fixes

--- cypress/integration/api_testing.js
+++ cypress/integration/api_testing.js
@@ -1,81 +1,91 @@
 import data from '../fixtures/data.json'
+import faker from 'faker'
+
+describe ("Souvenir-Memoir API Testing",()=>{
+        if (data.method=="GET"){
+            describe ('Testing Existing Data',()=>{
+                let id=""
+                it('Fetching All Memories and Check Status',()=>{
+                    cy.request({
+                        method:data.method,
+                        url:'/'
+                    }).then(res=> {
+                        id=res.body[0]._id
+                        console.log(id)
+                        expect(res.status).to.be.eq(200);
+                    })
+                })
+                it('Like Funcationality testing',()=>{
+                    cy.request({
+                        method:"PATCH",
+                        url:`/${id}/likePost`
+                    }).then(res=> {
+                        expect(res.body.title).to.be.eq("Chrollo Lucilfer");
+                        console.log(res.body)
+                    })
+                })
+                
+            });
+            }
+            else if (data.method=="POST"){
+            describe ('Checking CRUD Functionality',()=>{
+                let id=""
+                it('Creating New Memories',()=>{
+                    cy.fixture('data').then(Data =>{
+                    cy.request({
+                        method:data.method,
+                        url:'/',
+                        body: Data.payload
+                    }).then(res=> {
+                        cy.log(JSON.stringify(res.body))
+                        id=res.body._id;
+                        expect(res.body.title).to.be.eq(Data.payload.title);
+                        expect(res.body).to.have.property('creator')
+                    })
+                })
+                })
+                it('Assertion, The newly added id is present in data',()=>{
+                    cy.request({
+                        method:"GET",
+                        url:'/',
+                        timeout: 120000
+                    }).then(res=> {
+                        let arr= res.body;
+                        let userId=arr[arr.length - 1]._id;
+                        expect(userId).to.be.eq(id)
+                    })
+                })
+                it('Like the created memory',()=>{
+                    cy.request({
+                        method:"PATCH",
+                        url:`/${id}/likePost`
+                    }).then(res=> {
+                        expect(res.body.likeCount).to.be.eq(1)
+                    })
+                })
+                it('Update the created memory',()=>{
+                    cy.request({
+                        method:"PATCH",
+                        url:`/${id}`,
+                        body: data.editedPayload
+                    }).then(res=> {
+                        expect(res.body.title).to.be.eq(data.editedPayload.title);
+                        console.log(res.body)
+                    })
+                })
+                it('Delete the memory',()=>{
+                    cy.request({
+                        method:"DELETE",
+                        url:`/${id}`
+                    }).then(res=> {
+                        expect(res.body.message).to.be.eq("Post Deleted Successfully")
+                        console.log(res.body)
+                    })
+                })
+            
+            });
+            
+            }
+    })
 
 
-if (data.method=="GET"){
-describe ('Souvenior-Memoir-API-Testing',()=>{
-    let id=""
-    it('GET All Memories',()=>{
-        cy.request({
-            method:data.method,
-            url:'/'
-        }).then(res=> {
-            id=res.body[0]._id
-            console.log(id)
-        })
-    })
-    it('Like first memory',()=>{
-        cy.request({
-            method:"PATCH",
-            url:`/${id}/likePost`
-        }).then(res=> {
-            console.log(res.body)
-        })
-    })
-    
-});
-}
-else if (data.method=="POST"){
-describe ('Creating a post',()=>{
-    let id=""
-    it('Posting',()=>{
-        cy.request({
-            method:data.method,
-            url:'/',
-            body: data.payload
-        }).then(res=> {
-            id=res.body._id;
-            expect(res.body.title).to.be.eq(data.payload.title);
-           // expect(res.body).to.include(data.payload)
-        })
-    })
-    it('Assertion, The newly added id is present in data',()=>{
-        cy.request({
-            method:"GET",
-            url:'/'
-        }).then(res=> {
-            let arr= res.body;
-            let userId=arr[arr.length - 1]._id;
-            expect(userId).to.be.eq(id)
-        })
-    })
-    it('Like the created memory',()=>{
-        cy.request({
-            method:"PATCH",
-            url:`/${id}/likePost`
-        }).then(res=> {
-            expect(res.body.likeCount).to.be.eq(1)
-        })
-    })
-    it('Update the created memory',()=>{
-        cy.request({
-            method:"PATCH",
-            url:`/${id}`,
-            body: data.editedPayload
-        }).then(res=> {
-            expect(res.body.title).to.be.eq(data.editedPayload.title);
-            console.log(res.body)
-        })
-    })
-    it('Delete the memory',()=>{
-        cy.request({
-            method:"DELETE",
-            url:`/${id}`
-        }).then(res=> {
-            expect(res.body.message).to.be.eq("Post Deleted Successfully")
-            console.log(res.body)
-        })
-    })
-
-});
-
-}
